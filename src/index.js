04882f4cdc7dadb8a51efbbe888d809726f77d82
Pieter De Clercq <thepieterdc@users.noreply.github.com>
2022-05-20 19:10:37 +0200

    Add examples + fill whole (#2)
    Add examples

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const {parse, stringify} = require("svgson");
-exports.fillImage = async function (input, gradients, direction) {
+
+exports.fill = async function (input, color) {
+    // Parse the image.
+    const svg = await parse(input);
+
+    // Set the fill colour.
+    svg.children.forEach(c => {
+        c.attributes.fill = color;
+    });
+
+    return stringify(svg);
+}
+
+
+exports.fillGradient = async function (input, gradients, direction) {
     // Parse the image.
     const svg = await parse(input);
 
@@ -14,7 +28,7 @@ exports.fillImage = async function (input, gradients, direction) {
                 name: "linearGradient",
                 attributes: {
                     id: "gradientFill",
-                    gradientTransform: `rotate(${direction})`
+                    gradientTransform: `rotate(${direction || 0})`
                 },
                 children: gradients.flatMap(({color, from, to}) => [
                     {
